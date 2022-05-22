b72110b72cd0e976c7ddad5290b0d2f2963eccb3
kipukun ;_ <kipukun@waifu.club>
2022-05-21 23:31:37 -0400

    publishers: add base logic

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+)
+
+type bufWriteCloser struct {
+	*bufio.Writer
+}
+
+func (bwc *bufWriteCloser) Close() error {
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	publisherCmd := flag.NewFlagSet("publishers", flag.ExitOnError)
+	publisherFile := publisherCmd.String("f", "", "CSV list of manga titles to search for, leave empty for stdin")
+	publisherOutput := publisherCmd.String("o", "", "location of output file, leave empty for stdout")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected publishers or covers command")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "publishers":
+		publisherCmd.Parse(os.Args[2:])
+		var r io.Reader
+		if *publisherFile == "" {
+			r = os.Stdin
+		} else {
+			f, err := os.Open(*publisherFile)
+			if err != nil {
+				log.Fatalf("error opening file: %q", *publisherFile)
+			}
+			r = f
+		}
+		var w io.WriteCloser
+		if *publisherOutput == "" {
+			w = os.Stdout
+		} else {
+			wf, err := os.Create(*publisherOutput)
+			if err != nil {
+				log.Fatalf("error opening file: %q", *publisherFile)
+			}
+			w = wf
+		}
+
+		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+		defer cancel()
+
+		err := searchList(ctx, r, w)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	default:
+		fmt.Println("expected publishers or covers command")
+		os.Exit(1)
+	}
 }
